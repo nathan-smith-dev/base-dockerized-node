f0984711f0bbb508579e960b0bc658a2161d29d1
Nathan Smith <nathansmith@Nathans-iMac.local>
2019-08-03 09:06:35 -0400

    Added objection initialization

--- src/db/db.bootstrap.ts
+++ src/db/db.bootstrap.ts
@@ -1,11 +1,13 @@
 import { knex } from './knex';
 import { logger } from '../util/logger';
+import { Model } from 'objection';
 
 export class DbBootstrap {
   static async run() {
     logger.info('Bootstrapping db...');
     await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
     await this.runMigrations();
+    this.initObjection();
     logger.info('Bootstrapping db complete');
   }
 
@@ -13,5 +15,9 @@ export class DbBootstrap {
     logger.info('Running migrations...');
     await knex.migrate.latest();
     logger.info('Migrations run');
+  }
+
+  static initObjection() {
+    Model.knex(knex);
   }
 }
